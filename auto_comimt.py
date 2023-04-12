6a4aef728e1fb7428b4c8455d64540a6dcb44d79
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-04-12 09:49:55 +0700

    Fix typo

---
+++ auto_comimt.py
@@ -0,0 +1,16 @@
+import os
+import random
+
+# List of possible commit message prefixes
+prefixes = ['Fix', 'Update', 'Add', 'Remove', 'Refactor']
+
+# List of possible commit message suffixes
+suffixes = ['bug', 'typo', 'style', 'feature', 'doc']
+
+# Generate a random commit message
+prefix = random.choice(prefixes)
+suffix = random.choice(suffixes)
+message = f"{prefix} {suffix}"
+
+# Commit the changes with the generated message
+os.system(f"git add . && git commit -m '{message}'")
--- projects/scoresheets/index.py
+++ projects/scoresheets/index.py
@@ -12,7 +12,12 @@ print(getcwd())
 # print img shape
 print(img.shape)
 
+
+img = cvtColor(img, COLOR_BGR2GRAY)
+#
+retval, dst = threshold(img, 125, 255, THRESH_BINARY)
+
 # show result
-imshow('result', img)
+imshow('result', dst)
 waitKey(0)
 destroyAllWindows()
