6524b853b33a0f1e91c734d8b93eb3c46021d80a
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-04-12 10:01:18 +0700

    Refactor typo

--- projects/scoresheets/index.py
+++ projects/scoresheets/index.py
@@ -2,6 +2,33 @@ from cv2 import *
 from os import *
 
 
+# windown title
+window_title = 'result'
+
+# set name of windown
+namedWindow(window_title)
+
+
+def on_trackbar(val):
+    # function for track bar
+    img_processed = process_image(img, val)
+    # Show the processed image
+    img_processed = imshow(window_title, img_processed)
+    imshow(window_title, img_processed)
+
+
+def process_image(img, val):
+    # process
+
+    # convert to gray
+    img = cvtColor(img, COLOR_BGR2GRAY)
+
+    # threshold
+    retval, dst = threshold(img, val, 255, THRESH_BINARY)
+
+    return dst
+
+
 path = 'projects/scoresheets/input/test1.png'
 # read img from path
 img = imread(path)
@@ -13,11 +40,19 @@ print(getcwd())
 print(img.shape)
 
 
-img = cvtColor(img, COLOR_BGR2GRAY)
-#
-retval, dst = threshold(img, 125, 255, THRESH_BINARY)
+# Create the trackbar
+createTrackbar('parameter',
+               window_title,
+               0,
+               255,
+               on_trackbar)
 
-# show result
-imshow('result', dst)
+
+# Show the original image
+imshow(window_title, img)
+
+# Wait for a key press
 waitKey(0)
+
+# Destroy all windows
 destroyAllWindows()
