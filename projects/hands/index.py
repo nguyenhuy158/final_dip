06bcaa11f659511b49c9b1437859eebeae3225ae
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-04-13 23:52:26 +0700

    fix import

--- projects/hands/index.py
+++ projects/hands/index.py
@@ -1,59 +1,59 @@
-from cv2 import *
-from os import *
+import cv2
+import os
 
 
 # windown title
 window_title = "result"
 
 # set name of windown
-namedWindow(window_title)
+cv2.namedWindow(window_title)
 
 
 def on_trackbar(val):
     # function for track bar
     img_processed = process_image(img, val)
     # Show the processed image
-    img_processed = imshow(window_title, img_processed)
-    imshow(window_title, img_processed)
+    img_processed = cv2.imshow(window_title, img_processed)
+    cv2.imshow(window_title, img_processed)
 
 
 def process_image(img, val):
     # process
 
     # convert to gray
-    img = cvtColor(img, COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # threshold
-    img_processed = adaptiveThreshold(
-        img, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 11, 2
+    img_processed = cv2.adaptiveThreshold(
+        img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
     )
     return img_processed
 
 
 path = "projects/scoresheets/input/test1.png"
 # read img from path
-img = imread(path)
+img = cv2.imread(path)
 
 # resize img
-img = resize(img, (0, 0), fx=0.7, fy=0.7)
+img = cv2.resize(img, (0, 0), fx=0.7, fy=0.7)
 
 
 # print current working directory
-print(getcwd())
+print(os.getcwd())
 
 # print img shape
 print(img.shape)
 
 
 # Create the trackbar
-createTrackbar("parameter", window_title, 0, 255, on_trackbar)
+cv2.createTrackbar("parameter", window_title, 0, 255, on_trackbar)
 
 
 # Show the original image
-imshow(window_title, img)
+cv2.imshow(window_title, img)
 
 # Wait for a key press
-waitKey(0)
+cv2.waitKey(0)
 
 # Destroy all windows
-destroyAllWindows()
+cv2.destroyAllWindows()
