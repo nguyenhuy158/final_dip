7a139f117a7bb13d1ddb786e239d49637975edfc
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-05-12 11:18:51 +0700

    fix

--- projects/scoresheets/test.py
+++ projects/scoresheets/test.py
@@ -1,29 +1,54 @@
 import cv2
 import numpy as np
 
-# Load the image
-path = 'input/test1.png'
-# read img from path
-img = cv2.imread(path)
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+paths = ['input/test{}.jpg'.format(i) for i in range(1, 16)]
 
-# Threshold the image to create a binary image
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+def loop(path):
+    print(path)
+    
+    # Đọc ảnh
+    image = cv2.imread(path, 0)
 
-# Find contours in the binary image
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Áp dụng Gaussian Blur để giảm nhiễu
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
 
-# Iterate through each contour and check if it has the size and aspect ratio of a score cell
-for contour in contours:
-    x, y, w, h = cv2.boundingRect(contour)
-    aspect_ratio = float(w) / h
-    area = cv2.contourArea(contour)
-    if area > 100 and area < 500 and aspect_ratio > 0.8 and aspect_ratio < 1.2:
-        # This contour is a score cell
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Dùng Hough Circle Transform để phát hiện các vòng tròn
+    circles = cv2.HoughCircles(blurred, 
+                            cv2.HOUGH_GRADIENT, 
+                            1, 
+                            20,
+                            param1=50, 
+                            param2=30, 
+                            minRadius=0, 
+                            maxRadius=20)
 
-# Display the result
-cv2.imshow("Result", img)
-cv2.waitKey(0)
+    # Nếu tìm thấy các vòng tròn
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        
+        # Vẽ các vòng tròn đã tìm thấy lên ảnh gốc
+        for (x, y, r) in circles:
+            cv2.circle(image, (x, y), r, (0, 255, 0), 2)
+
+    else:
+        print("Không tìm thấy vòng tròn trong ảnh")
+
+
+def imshow(image):
+    # Hiển thị ảnh gốc và ảnh đã tìm thấy các vòng tròn
+    cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+    cv2.imshow('Image', image)
+
+    # Thay đổi kích thước cửa sổ hiển thị ảnh
+    cv2.resizeWindow('Image', 800, 600)
+
+    # Chờ bấm phím bất kỳ để thoát
+    cv2.waitKey(0)
+
+    # Giải phóng bộ nhớ
+    cv2.destroyAllWindows()
+
+    
+for path in paths:
+    loop(path)
